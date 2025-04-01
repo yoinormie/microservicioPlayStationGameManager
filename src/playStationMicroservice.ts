9fd40acdf82cc68a3d3761e9475c151745c92fe8
Yoel García Lago <yoelgarcialago@gmail.com>
2025-04-01 11:53:41 +0200

    Clave de acceso obtenida

--- src/playStationMicroservice.ts
+++ src/playStationMicroservice.ts
@@ -1,6 +1,17 @@
 import express = require('express');
 import app from './config';
 import NpssoKey from './config'
+import psn = require('psn-api')
+
+
+//const accessCode = await psn.exchangeNpssoForAccessCode(NpssoKey.toString());
+async function exchangeKey4AccessCode() {
+    const accessCode = await psn.exchangeNpssoForAccessCode(String(process.env.MyNpsso));
+    return accessCode;
+}
+
+const accessCode = exchangeKey4AccessCode()
+
 
 app.get('/', (req: express.Request, res: express.Response) => {
     res.json({
