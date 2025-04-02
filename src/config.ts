b678ea682c08c873419bc354daedfa0c90f5cdcd
Yoel García Lago <yoelgarcialago@gmail.com>
2025-04-02 10:37:50 +0200

    Clases documentadas

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 import express = require('express') 
-
+/**
+ * Variable que define qué va a usar el servidor (en este caso express con json) y la exporta para que la usen otras clases
+ */
 const app = express()
 
 app.use(express.json())
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import app from './config'
 import './playStationMicroservice'
-
+/**
+ * Clase que define el puerto por el que va a escuchar el servidor y lo activa, mandando un mensaje por consola
+ * También importa la clase playStationMicroservice para que acepte los endpoints
+ */
 const port = 3050
 
 app.listen(port, () => {
