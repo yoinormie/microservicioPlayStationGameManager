15e6f60651fb80eee6d65160e3f8a78e7919bdc8
YoelGarciaLago <yoelgarcialago@gmail.com>
2025-04-25 18:37:13 +0200

    Server modificado

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import dotenv from 'dotenv';
 dotenv.config()
 import app from './Config/config'
-import './Controller/playStationMicroservice'
 /**
  * Clase que define el puerto por el que va a escuchar el servidor y lo activa, mandando un mensaje por consola
  * También importa la clase playStationMicroservice para que acepte los endpoints
  */
 
-const port = process.env.PORT;
+const port = process.env.PORT
+const commonURLEndpoint = '/playStation'
 
 app.listen(port, () => {
     console.log(`Microservicio iniciado en el puerto ${port}`)
