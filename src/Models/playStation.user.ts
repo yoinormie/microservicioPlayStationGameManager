2f097cd4471ed4950d35889b72e4b53d730cce16
YoelGarciaLago <yoelgarcialago@gmail.com>
2025-05-05 09:40:36 +0200

    Clase para definir a un usuario de PlayStation

--- src/Models/playStation.user.ts
+++ src/Models/playStation.user.ts
@@ -1,12 +1,33 @@
 /**
  * Clase que define a un usuario con sus características 
  */
-export class userPlayStationNetwork{
+export class userPlayStationNetwork {
+    private onlineID: string
+    private accountID: string
+    private country: string
+    private language: string
+    private isPSPlus: boolean
     constructor(
-        onlineID : string,
-        accountID : string,
-        country : string,
-        language : string,
-        isPSPlus : boolean
-    ){}
+        onlineID: string,
+        accountID: string,
+        country: string,
+        language: string,
+        isPSPlus: boolean
+    ) {
+        this.onlineID = onlineID
+        this.accountID = accountID
+        this.country = country
+        this.language = language
+        this.isPSPlus = isPSPlus
+    }
+
+    public toString() {
+        return {
+            onlineID: this.onlineID,
+            accountID: this.accountID,
+            country: this.country,
+            language: this.language,
+            isPSPlus: this.isPSPlus,
+        }
+    };
 }
