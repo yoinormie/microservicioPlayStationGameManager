a4d79fa0008955566b46d93e72b404ffc8c6ccf2
YoelGarciaLago <yoelgarcialago@gmail.com>
2025-05-05 09:41:49 +0200

    Definido la url común y usada la ruta para los endPoints

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config()
 import app from './Config/config'
+import route from './Routes/playstation.route';
 /**
  * Clase que define el puerto por el que va a escuchar el servidor y lo activa, mandando un mensaje por consola
  * También importa la clase playStationMicroservice para que acepte los endpoints
@@ -12,3 +13,5 @@ const commonURLEndpoint = '/playStation'
 app.listen(port, () => {
     console.log(`Microservicio iniciado en el puerto ${port}`)
 })
+
+app.use(commonURLEndpoint, route)
