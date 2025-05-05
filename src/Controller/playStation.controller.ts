d3d16997e3178610e303aa67a2382c4fe6a5255a
YoelGarciaLago <yoelgarcialago@gmail.com>
2025-05-05 09:40:04 +0200

    Controllers acabados (falta la conectividad con Spring Boot)

--- src/Controller/playStation.controller.ts
+++ src/Controller/playStation.controller.ts
@@ -2,9 +2,9 @@ import {Response, Request} from 'express';
 import {userPlayStationNetwork} from '../Models/playStation.user'
 import { getAccountName, getAccountGames } from '../Services/playstation.service'
 import * as psn from 'psn-api';
-import { searchUserANSchema } from '../Schemas/playStation.shema';
+import { searchUserANSchema,  getGamesWithAIDSchema} from '../Schemas/playStation.shema';
 
-const accessCode = await psn.exchangeNpssoForAccessCode(process.env.MYNPSSO as string)
+const accessCode = await psn.exchangeNpssoForAccessCode("PIMCgzdsGiw9BTu2cFlGGGsLfy70QmtnoZI21rCN8MX3fjjMLcMO1QHdoJ1qtwvm")
 const authorization = await psn.exchangeAccessCodeForAuthTokens(accessCode)
 
 export const getUserWithAN = async (req: Request, res: Response) => {
@@ -13,6 +13,19 @@ export const getUserWithAN = async (req: Request, res: Response) => {
         var username = bodyParsed.data.username
         const userToSend = await getAccountName(username, authorization)
         //Llamada a microservicio de spring boot y finaliza
+        res.json(userToSend)
     }
 
+}
+
+export const getGamesWithAID = async (req: Request, res: Response) => {
+    let bodyParsed = getGamesWithAIDSchema.safeParse(req.body)
+    if(bodyParsed.success){
+        const envolvedLists = await getAccountGames(bodyParsed.data.accountID, authorization)
+        const gamesList = envolvedLists.getUserGames()
+        const userPropertyGames = envolvedLists.getUserPropertyGames()
+        //Llamada a los endpoints de spring boot y finaliza
+        res.json(gamesList)
+    }
+    
 }
