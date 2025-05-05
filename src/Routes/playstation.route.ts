afcbd83fd641ad420910feb3d911717f2c4fd86b
YoelGarciaLago <yoelgarcialago@gmail.com>
2025-05-05 09:40:57 +0200

    Ruta para la obtención de los juegos hecha

--- src/Routes/playstation.route.ts
+++ src/Routes/playstation.route.ts
@@ -1,11 +1,11 @@
 import { Router } from "express"
-import {getUserWithAN} from '../Controller/playStation.controller'
+import {getUserWithAN, getGamesWithAID} from '../Controller/playStation.controller'
 import { ValidateBody } from "../Middleware/playStation.middleware"
-import {searchUserANSchema} from '../Schemas/playStation.shema'
+import {searchUserANSchema, getGamesWithAIDSchema} from '../Schemas/playStation.shema'
 import app from "../Config/config"
 
 const route = Router();
 route.post('/searchuserwithan', ValidateBody(searchUserANSchema), getUserWithAN);
-route.post('/searchgameswithaid',);
+route.post('/searchgameswithaid', ValidateBody(getGamesWithAIDSchema), getGamesWithAID);
 
 export default route;
